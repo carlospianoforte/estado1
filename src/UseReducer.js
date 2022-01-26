37089801fe8b3abc00211e867bc48506f8308c43
Carlos Ortiz <carlospianiforte@gmail.com>
2022-01-25 23:13:54 -0500

    comit_usereducer

---
+++ src/UseReducer.js
@@ -0,0 +1,167 @@
+import React from 'react'
+
+const SECURITY_CODE = '123';
+
+const UseReducer = (props) => {
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+
+    cons
+
+    React.useEffect(() =>{
+
+        if(!!state.loading){
+ 
+            setTimeout(() => {
+                console.log('validando...');
+                if(state.value === SECURITY_CODE){
+
+                    dispatch({
+                        type: actionTypes.confirm
+                    });
+
+                }else{
+                    
+                    dispatch({
+                        type: actionTypes.error
+                    });
+                }
+
+            }, 2000);
+        }
+
+    }, [state.loading]);
+
+    if(!state.deleted && !state.confirmed){
+        return (
+            <div>
+                <h2>Eliminar {props.name}</h2>
+
+                <p>Escribe codigo de seguridad</p>
+                
+                {(state.error) && (<p>Código incorrecto</p>)}
+
+                {state.loading && <p>Cargando...</p>}
+
+                <input 
+                    placeholder='Código de seguridad' 
+                    type="text"
+                    value={state.value}
+                    onChange={(e) => {
+                        dispatch({type: actionTypes.write, payload: e.target.value});
+                }}
+                />
+
+                <button 
+                    onClick={() => {
+                        dispatch({type: actionTypes.check});
+                    }}
+                    >
+                        Comprobar
+                </button>
+            </div>
+        );
+    }else if (!!state.confirmed && !state.deleted){
+        return(
+            <>
+                <p>Estado confirmacion. ¿Estas seguro?</p>
+                <button 
+                    onClick={()=>{
+                        dispatch({type: actionTypes.delete});
+                    }}
+                >
+                        Si, eliminar
+                </button>
+
+                <button 
+                    onClick={()=>{
+                        dispatch({type: actionTypes.reset});
+                    }}
+                >
+                        No, volver
+                </button>
+
+
+            </>
+        );
+
+    }else{
+        return(
+            <>
+                <p>Eliminado con exito!</p>
+                <button 
+                    onClick={()=>{
+                        dispatch({type: actionTypes.reset});
+                    }}
+                >
+                        Resetear, volver atrás
+                </button>
+            </>
+        );
+    }
+        
+}
+
+
+const initialState ={
+    value: '',
+    error: false,
+    loading: false,
+    deleted: false,
+    confirmed: false,
+};
+
+const actionTypes = {
+    confirm: 'CONFIRM',
+    delete: 'DELETE',
+    reset: 'RESET',
+    check: 'CHECK',
+    write: 'WRITE',
+    error: 'ERROR',
+};
+
+const reducerObjet = (state, payload) => ({
+    [actionTypes.error]: {
+        ...state,
+        loading: false,
+        error: true,
+    },
+    [actionTypes.check]: {
+        ...state,
+        loading: true,
+    },
+    [actionTypes.write]: {
+        ...state,
+        value: payload,
+    },
+    [actionTypes.confirm]: {
+        ...state,
+        loading: false,
+        error: false,
+        confirmed: true,
+    },
+    [actionTypes.delete]:{
+        ...state,
+        deleted: true,
+    },
+    [actionTypes.reset]: {
+        ...state,
+        deleted:false,
+        confirmed:false,
+        value: '',
+    },
+});
+
+const reducer = (state, action) => {
+    if(reducerObjet(state)[action.type]){
+        return reducerObjet(state, action.payload)[action.type];
+    } else {
+        return state;
+    }
+};
+
+export { UseReducer };
+
+
+
+
+         
